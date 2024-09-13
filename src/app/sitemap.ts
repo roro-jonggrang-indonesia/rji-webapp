9eb4da529d950b3a8f72eea7a7b658253f519fe7
genzo0 <m.alifyan02@gmail.com>
2024-09-13 23:20:29 +0700

    add seo file and change link a little bit

---
+++ src/app/sitemap.ts
@@ -0,0 +1,49 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const [articlesRes, servicesRes, portfoliosRes] = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/articles`),
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/services`),
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/portfolios`),
+  ]);
+
+  const [{ data: articles }, { data: services }, { data: portfolios }] =
+    await Promise.all([
+      articlesRes.json(),
+      servicesRes.json(),
+      portfoliosRes.json(),
+    ]);
+
+  const articleEntries = articles.map((article: any) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/artikel/${article.id}`,
+  }));
+
+  const servicesEntries = services.map((service: any) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/services/${service.id}`,
+  }));
+
+  const portfoliosEntries = portfolios.map((portfolio: any) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/portfolios/${portfolio.id}`,
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/artikel`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/layanan`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/portofolio`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/tentang-kami`,
+    },
+    ...portfoliosEntries,
+    ...servicesEntries,
+    ...articleEntries,
+  ];
+}
